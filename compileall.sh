33244da0b0923ad747a34fc0fa56d375620a173c
Gerald Pape <mail@geraldpape.io>
2020-09-29 10:45:05 +0200

    remove bib stuff & silence exit code of compileall

--- compileall.sh
+++ compileall.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
 # Compile document
-pdflatex -interaction=nonstopmode main
-
-# Compile nomenclature
-makeindex main.nlo -s nomencl.ist -o main.nls
-
-# Compile index
-makeindex main
-
-# Compile bibliography
-biber main
-
-# Compile document
-pdflatex -interaction=nonstopmode main
-
-# Compile glossary
-makeglossaries main
-
-# Compile document
-pdflatex -interaction=nonstopmode main
+pdflatex -interaction=nonstopmode main.tex || true
