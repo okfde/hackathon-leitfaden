a42ac264475ba717304a7b52e97c191a25d43a78
Gerald Pape <mail@geraldpape.io>
2020-09-30 10:23:33 +0200

    include tagged version in pdf

--- compileall.sh
+++ compileall.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+version=${DOCUMENT_VERSION:-}
+if [[ -z "$version" ]]; then
+  echo "Couldn't find required \$DOCUMENT_VERSION environment variable"
+  echo "exiting ..."
+  exit 1
+fi
+
+target="vREPLACEME"
+
+grep -q "$target" main.tex
+if [ $? -ne 0 ]
+then
+  echo "Couldn't find required string $target in main.tex"
+  echo "exiting ..."
+  exit 1
+fi
+
+echo "replacing $target with $version in main.tex"
+sed -i "s|$target|$version|g" main.tex
+
 # Compile document
 pdflatex -interaction=nonstopmode main.tex || true
 
