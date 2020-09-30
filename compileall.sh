eb4b554e18d8d0b449636e8c09e4483f2765388e
Gerald Pape <mail@geraldpape.io>
2020-09-30 09:47:13 +0200

    execute pdflatex twice for toc

--- compileall.sh
+++ compileall.sh
@@ -2,3 +2,6 @@
 
 # Compile document
 pdflatex -interaction=nonstopmode main.tex || true
+
+# Compile document a second time to include the table of contents
+pdflatex -interaction=nonstopmode main.tex || true
